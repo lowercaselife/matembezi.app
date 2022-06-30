fb0c167f0559edd5d6805fd054ab3821f6fe7de2
lowercaselife <dansonkalaghe@gmail.com>
2022-06-30 23:31:44 +0300

    api v1 and controlers

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const fs = require('fs');
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
+const app = express();
+
+// 1) MIDDLEWARES
+app.use(morgan('dev'));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log('hello, from the middleware');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+
+// 3) R     OUTES
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+// 4) START SERVER
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
