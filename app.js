abceef108af1ff015b0cf1b6828501e9cca86e68
lowercaselife <dansonkalaghe@gmail.com>
2022-07-01 23:35:26 +0300

    eslint and dev dependencies added

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const exp = require('constants');
+//const exp = require('constants');
 const express = require('express');
-const fs = require('fs');
+//const fs = require('fs');
 const morgan = require('morgan');
 
 const tourRouter = require('./routes/tourRoutes');
@@ -9,11 +9,13 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1) MIDDLEWARES
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use(express.json());
 
-app.use(express.static(`${__dirname}/public`))
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('hello, from the middleware');
