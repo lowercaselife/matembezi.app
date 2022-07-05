5e8a8edc6c9a02e5667a77939ecd477fe7ae6a69
lowercaselife <dansonkalaghe@gmail.com>
2022-07-05 20:36:34 +0300

    advanced filtering snd sorting

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,12 +2,32 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
+    console.log(req.query);
+
+    // BUILD QUERY
+    // 1) Filtering
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    const tours = await Tour.find(queryObj);
+    // 1b.) Advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
+    let query = Tour.find(JSON.parse(queryStr));
+
+    //2 Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    // EXCECUTE QUERY
+    const tours = await query;
+
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       results: tours.length,
