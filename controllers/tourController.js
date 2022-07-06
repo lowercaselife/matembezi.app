642c4ee3b5acc77313cf6da58edd105f472df7ac
lowercaselife <dansonkalaghe@gmail.com>
2022-07-06 14:11:05 +0300

    API  pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -24,6 +24,25 @@ exports.getAllTours = async (req, res) => {
       query = query.sort('-createdAt');
     }
 
+    // 4) Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+
+    //3) Field limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
     // EXCECUTE QUERY
     const tours = await query;
 
