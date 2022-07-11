60cd5d647a57ab99c005560bd4d0dabb4e7302f5
lowercaselife <dansonkalaghe@gmail.com>
2022-07-12 01:01:20 +0300

    protection against common attacks using middleware

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
-
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -9,15 +12,35 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// 1) MIDDLEWARES
+// GLOBAL MIDDLEWARES
+// set security http headers
+app.use(helmet());
+
+// development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+// limit amount of requests from one IP
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour',
+});
+app.use('/api', limiter);
 
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+//Data sanitization against cross site scripting
+app.use(xss());
+
+// body parser, reading data from body into req.body
 app.use(express.json());
 
+// serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
