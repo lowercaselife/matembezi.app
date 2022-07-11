bcf78ba57960307fd92527ca031f83ce551cb563
lowercaselife <dansonkalaghe@gmail.com>
2022-07-11 23:21:35 +0300

    add jwt token to cookie

--- models/userModel.js
+++ models/userModel.js
@@ -41,6 +41,11 @@ const userSchema = new mongoose.Schema({
   passwordChangedAt: Date,
   passwordResetToken: String,
   passwordResetExpires: Date,
+  active: {
+    type: Boolean,
+    default: true,
+    select: false,
+  },
 });
 
 //middleware to encrypt password
@@ -61,6 +66,13 @@ userSchema.pre('save', function (next) {
   if (!this.isModified('password') || this.isNew) return next();
 
   this.passwordChangedAt = Date.now() - 1000;
+  next();
+});
+
+// Disable find() on deactivated accounts
+userSchema.pre(/^find/, function (next) {
+  // this points to the current query
+  this.find({ active: { $ne: false } });
   next();
 });
 
